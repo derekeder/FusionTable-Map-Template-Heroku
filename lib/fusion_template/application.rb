2c72a6673f6657bdb73c3fc3c0abf64caa8083fd
Derek Eder <derek.eder@gmail.com>
2012-12-23 09:38:01 -0600

    added memcache hook, robots.txt, 404 not found

--- lib/fusion_template/application.rb
+++ lib/fusion_template/application.rb
@@ -23,8 +23,12 @@ module FusionTemplate
     use Rack::Session::Cookie
 
     helpers FusionTemplate::HtmlHelpers
+
+    FusionTableId = '1m4Ez9xyTGfY2CU6O-UgEcPzlS0rnzLU93e4Faa0'
     
     get "/" do
+      # uncomment this line to cache this route
+      # cache_control :public, max_age: 1800  # 30 min
       @current_menu = "home"
       haml :index
     end
@@ -34,16 +38,27 @@ module FusionTemplate
       @current_menu = "location_list"
       if defined? FT
         # note: the fusion_tables gem can only access tables based on the numeric ID of the table
-        @recycling_locations = FT.execute("SELECT * FROM 2086698;")
+        @recycling_locations = FT.execute("SELECT * FROM #{FusionTableId};")
         haml :location_list
       else
         "fusion_tables gem not setup yet! You need to set your Google account and password in config/config.yml"
       end
     end
     
-    get "/:page" do
-      @current_menu = params[:page]
-      haml params[:page].to_sym
+    # catchall for static pages
+    get "/:page/?" do
+      begin 
+        @current_menu = params[:page]
+        @title = params[:page].capitalize.gsub(/[_-]/, " ") + " - Searchable Map Template - Heroku Ready"
+        @page_path = params[:page]
+        haml params[:page].to_sym
+      rescue Errno::ENOENT
+        haml :not_found
+      end
+    end
+
+    error do
+      'Sorry there was a nasty error - ' + env['sinatra.error'].name
     end
   end
 end
